a37ec2c2aee4cecbd2c6a7d2ac80347e51125937
Marvin Li <marvin.fangzhou.li@gmail.com>
2023-04-29 09:03:36 -0400

    perturbation attack + cleaning up

--- cross_entropy_threshold_attack.py
+++ cross_entropy_threshold_attack.py
@@ -10,6 +10,7 @@ import json
 from datasets import load_dataset
 from transformers import GPTNeoXForCausalLM, AutoTokenizer
 from transformers.optimization import AdamW
+from common_code import *
 
 # %%
 model_title = "pythia-6.9B-deduped"
@@ -18,15 +19,6 @@ nbatches = 500 ## Number of batches to gather data on. Number of data points is 
 samplelength = 50 ## How long are the sequences we take from the training and validation sets.
 print(model_title, bs, nbatches, samplelength)
 ####################################################################################
-# %%
-## Memory statistics. I had to be careful with cuda memory 
-def mem_stats():
-    t = torch.cuda.get_device_properties(0).total_memory
-    r = torch.cuda.memory_reserved(0)
-    a = torch.cuda.memory_allocated(0)
-    print("total memory: ", t, "reserve mem: ", r, "total-reserved:", t-r, "allocated:", a,"\n (total-reserved)/total:", (t-r)/t,"(reserved-allocated)/reserved:", (r-a)/r)
-    return 
-
 
 # %%
 ## Model and tokenizer
@@ -79,73 +71,17 @@ mem_stats()
 
 # %%
 ## Dataloader 
+torch.manual_seed(124)
 training_dataloader = DataLoader(training_dataset, batch_size = bs, collate_fn=collate_already_encoded, shuffle = True)
 validation_dataloader = DataLoader(validation_dataset, batch_size = bs, collate_fn=collate_fn, shuffle = True)
 mem_stats()
 
 # %%
-## Training dataset information
-from torch.nn import CrossEntropyLoss
-def compute_input_ids_cross_entropy(model, input_ids):
-  mask  = (input_ids > 0).detach()                                     
+train_cross_entropy = compute_dataloader_cross_entropy(training_dataloader, nbatches, bs, device, model, samplelength)
+val_cross_entropy = compute_dataloader_cross_entropy(validation_dataloader, nbatches, bs, device, model, samplelength)
 
-  model.train(False)
 
-  with torch.no_grad():
-    outputs = model(input_ids=input_ids.to(torch.long), attention_mask = mask)
-    logits = outputs.logits
-
-  loss_fn = CrossEntropyLoss()
-  input_len = input_ids.shape[-1] - 1
-  input_ids_without_first_token = input_ids[:, 1:].long()
-  logits_without_last_token = logits[:, :-1, :]
-
-  # print(input_ids_without_first_token)
-  ans = []
-  for i in range(len(logits_without_last_token)):
-    length = len(input_ids_without_first_token[i,:])
-    if len(torch.where(input_ids_without_first_token[i,:] == 0)[0]) > 0:
-      length = torch.where(input_ids_without_first_token[i,:] == 0)[0].min()
-    # print(logits_without_last_token[i, :length].shape)
-    # print(input_ids_without_first_token[i, :length].shape)
-    # print(loss_fn(logits_without_last_token[i, :length], input_ids_without_first_token[i, :length]))
-    ce_loss = loss_fn(logits_without_last_token[i, :length], input_ids_without_first_token[i, :length])
-    ans.append(ce_loss/length)
-
-  ## Clean up 
-  del outputs, logits, input_ids_without_first_token, logits_without_last_token
-  torch.cuda.empty_cache()
-  torch.cuda.synchronize()
-
-  return torch.Tensor(ans)
-
-def compute_cross_entropy(dataloader, nbatches, bs, device, model, samplelength):    
-    cross_entropy = torch.zeros((nbatches, bs))
-    for batchno, data_x in enumerate(dataloader):
-        if batchno >= nbatches:
-            break
-        with torch.no_grad():       
-            ## Get predictions on training data                       
-            data_x = data_x[:,:samplelength].to(device).detach()
-   
-            ## Compute average log likelihood
-            cross_entropy[batchno, :] = compute_input_ids_cross_entropy(model, data_x)
-
-        if batchno % 50 == 0:
-            print("batch no. ", batchno)  
-            print("Memory after training")
-            mem_stats()
-            print()
-    return cross_entropy
-
-train_cross_entropy = compute_cross_entropy(training_dataloader, nbatches, bs, device, model, samplelength)
-val_cross_entropy = compute_cross_entropy(validation_dataloader, nbatches, bs, device, model, samplelength)
-
-# %%
-import matplotlib.pyplot as plt
-import numpy as np
-
-# generate two sets of random values
+## Save outputs
 with torch.no_grad():
     valuestraining   = torch.flatten(train_cross_entropy) 
     valuesvalidation = torch.flatten(val_cross_entropy)
@@ -154,67 +90,18 @@ notnan = torch.logical_and(~valuestraining.isnan(), ~valuesvalidation.isnan())
 valuestraining = valuestraining[notnan]
 valuesvalidation = valuesvalidation[notnan]
 
-    
+
+## save as pt file
 torch.save(torch.vstack((valuestraining, valuesvalidation)), 
 model_title + " (Training, Validation) data: bs=" + str(bs)+", nbatches="+str(nbatches)+", length="+str(samplelength)+").pt")
 
-# create a figure and axis object
-plt.figure()
-fig, ax = plt.subplots()
+plot_hist(train_cross_entropy, val_cross_entropy, show_plot = True, 
+            save_plot=True, 
+            plot_title = model_title + " Histogram (bs=" + str(bs)+", nbatches="+str(nbatches)+", length="+str(samplelength)+")",
+            plot_name=model_title + " Histogram (bs=" + str(bs)+", nbatches="+str(nbatches)+", length="+str(samplelength)+").png")
 
-# plot a histogram of the first set of values with 20 bins
-ax.hist(valuestraining, bins=20, alpha=0.5, label='training')
-
-# plot a histogram of the second set of values with 20 bins
-ax.hist(valuesvalidation, bins=20, alpha=0.5, label='validation')
-
-# add a legend to the plot
-ax.legend(loc='upper right')
-
-# add labels and a title to the plot
-ax.set_xlabel('Value')
-ax.set_ylabel('Frequency')
-ax.set_title(model_title + " Histogram (bs=" + str(bs)+", nbatches="+str(nbatches)+", length="+str(samplelength)+")")
-
-# show the plot
-
-plt.savefig(model_title + " Histogram (bs=" + str(bs)+", nbatches="+str(nbatches)+", length="+str(samplelength)+").png")
-
-
-# %%
-from sklearn.metrics import roc_curve, auc
-
-## Scale all values to be between 0 and 1
-st = min(valuestraining.min(),valuesvalidation.min())
-end = max(valuestraining.max(),valuesvalidation.max())
-print(st,end)
-
-
-y_scores =  torch.cat((valuestraining, valuesvalidation))
-y_scores = y_scores-min(y_scores)
-y_scores = y_scores/max(y_scores)
-y_true   = [1 for _ in range(len(valuestraining))] + [0 for _ in range(len(valuesvalidation))]
-
-fpr, tpr, thresholds = roc_curve(y_true, y_scores)
-
-# Calculate the area under the ROC curve (AUC)
-roc_auc = auc(fpr, tpr)
-
-# Plot the ROC curve
-plt.figure()
-plt.plot(fpr, tpr, color='darkorange', label='ROC curve (area = %0.2f)' % roc_auc)
-plt.plot([0, 1], [0, 1], color='navy', linestyle='--')
-plt.xlim([0.0, 1.0])
-plt.ylim([0.0, 1.05])
-plt.xlabel('False Positive Rate')
-plt.ylabel('True Positive Rate')
-plt.title(model_title + " ROC (bs=" + str(bs)+", nbatches="+str(nbatches)+", length="+str(samplelength)+").png")
-plt.legend(loc="lower right")
-plt.savefig(model_title + " ROC (bs=" + str(bs)+", nbatches="+str(nbatches)+", length="+str(samplelength)+").png")
-
-
-
-# %%
-
+plot_ROC(train_cross_entropy, val_cross_entropy, show_plot = True, save_plot = False, log_scale = False, 
+        plot_title =model_title + " ROC (bs=" + str(bs)+", nbatches="+str(nbatches)+", length="+str(samplelength)+")", 
+        plot_name = model_title + " ROC (bs=" + str(bs)+", nbatches="+str(nbatches)+", length="+str(samplelength)+").png")
 
 
