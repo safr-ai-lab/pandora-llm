a1c9ffef8b3c6efc901b37a24cb1d62fb4a9520a
Seth Neel <sethneel93@gmail.com>
2023-06-27 23:54:01 -0400

    Create setup.sh
    add bash script that installs packages

---
+++ setup.sh
@@ -0,0 +1,9 @@
+pip install transformers
+pip install datasets
+pip install zstandard
+pip install deepspeed
+pip install accelerate
+pip install mpi4py
+pip install scikit-learn # for ROC curves
+pip install matplotlib # for plotting
+pip install plotly # for plotting MoPe vs LOSS (dynamic)
