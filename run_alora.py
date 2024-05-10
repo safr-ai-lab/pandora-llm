4e8012c42ea5c73422fcb52f8afe75316cb6ed23
jsonW0 <jasonwang1@college.harvard.edu>
2024-05-09 22:23:25 -0400

    working alora

---
+++ run_alora.py
@@ -0,0 +1,130 @@
+import time
+import math
+import argparse
+import torch
+from torch.utils.data import DataLoader
+from transformers import AutoTokenizer, AutoConfig
+from src.utils.attack_utils import *
+from src.utils.dataset_utils import *
+from src.utils.log_utils import get_my_logger
+from src.attacks.ALoRa import ALoRa
+from accelerate import Accelerator
+from accelerate.utils import set_seed
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+"""
+Sample command line prompt (no acceleration)
+python run_alora.py --model_name EleutherAI/pythia-70m-deduped --model_revision step98000 --n_samples 1000 --pack --seed 229
+Sample command laine prompt (with acceleration)
+accelerate launch run_alora.py --accelerate --model_name EleutherAI/pythia-70m-deduped --model_revision step98000 --n_samples 1000 --pack --seed 229
+"""
+
+def main():
+    ####################################################################################################
+    # SETUP
+    ####################################################################################################
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--experiment_name', action="store", type=str, required=False, help='Experiment name. Used to determine save location.')
+    # Model Arguments
+    parser.add_argument('--model_name', action="store", type=str, required=True, help='Huggingface model name')
+    parser.add_argument('--model_revision', action="store", type=str, required=False, help='Model revision. If not specified, uses main.')
+    parser.add_argument('--model_cache_dir', action="store", type=str, required=False, help='Model cache directory. If not specified, uses main.')
+    # Dataset Arguments
+    parser.add_argument('--num_samples', action="store", type=int, required=True, help='Dataset size')
+    parser.add_argument('--bs', action="store", type=int, required=False, default=1, help='Batch size')
+    parser.add_argument('--min_length', action="store", type=int, required=False, default=20, help='Min number of tokens (filters)')
+    parser.add_argument('--max_length', action="store", type=int, required=False, help='Max number of tokens (truncates)')
+    parser.add_argument('--pack', action="store_true", required=False, help='Pack validation set')
+    parser.add_argument('--unpack', action="store_true", required=False, help='Unpack training set')
+    parser.add_argument('--train_pt', action="store", required=False, help='.pt file of train dataset (not dataloader)')
+    parser.add_argument('--val_pt', action="store", required=False, help='.pt file of val dataset (not dataloader)')
+    # Attack Arguments
+    parser.add_argument('--lr', action="store", type=float, required=False, default=5e-05, help='Learning rate. Deafult is 5e-05.')
+    # Device Arguments
+    parser.add_argument('--seed', action="store", type=int, required=False, default=229, help='Seed')
+    parser.add_argument('--accelerate', action="store_true", required=False, help='Use accelerate')
+    parser.add_argument('--model_half', action="store_true", required=False, help='Use half precision (fp16). 1 for use; 0 for not.')
+    args = parser.parse_args()
+    
+    accelerator = Accelerator() if args.accelerate else None
+    set_seed(args.seed)
+    args.model_cache_dir = args.model_cache_dir if args.model_cache_dir is not None else f"models/{args.model_name.replace('/','-')}"
+    args.experiment_name = args.experiment_name if args.experiment_name is not None else ALoRa.get_default_name(args.model_name,args.model_revision,args.num_samples,args.seed)
+    logger = get_my_logger(log_file=f"{args.experiment_name}.log")
+    ####################################################################################################
+    # LOAD DATA
+    ####################################################################################################
+    start = time.perf_counter()
+
+    max_length = AutoConfig.from_pretrained(args.model_name).max_position_embeddings
+    tokenizer = AutoTokenizer.from_pretrained(args.model_name)
+    
+    logger.info("Loading Data")    
+
+    if not (args.pack ^ args.unpack):
+        logger.info(f"WARNING: for an apples-to-apples comparison, we recommend setting exactly one of pack ({args.pack}) and unpack ({args.unpack})")
+    
+    # Load training data
+    if args.train_pt:
+        logger.info("You are using a self-specified training dataset...")
+        fixed_input = args.train_pt + ".pt" if not args.train_pt.endswith(".pt") else args.train_pt
+        training_dataset = torch.load(fixed_input)[:args.num_samples]
+        training_dataloader = DataLoader(training_dataset, batch_size = args.bs, collate_fn=lambda batch: collate_fn(batch, tokenizer=tokenizer, max_length=max_length))
+    else:
+        training_dataset = load_train_pile_random(number=args.num_samples,seed=args.seed,num_splits=1,min_length=args.min_length,deduped="deduped" in args.model_name,unpack=args.unpack)[0]
+        training_dataloader = DataLoader(training_dataset, batch_size = args.bs, collate_fn=lambda batch: collate_fn(batch, tokenizer=tokenizer, max_length=max_length))
+
+    # Load validation data
+    if args.val_pt:
+        fixed_input = args.val_pt + ".pt" if not args.val_pt.endswith(".pt") else args.val_pt
+        logger.info("You are using a self-specified validation dataset...")
+        validation_dataset = torch.load(fixed_input)[:args.num_samples]
+        validation_dataloader = DataLoader(validation_dataset, batch_size = args.bs, collate_fn=lambda batch: collate_fn(batch, tokenizer=tokenizer, max_length=max_length))
+    else:
+        validation_dataset = load_val_pile(number=args.num_samples, seed=args.seed, num_splits=1, window=2048 if args.pack else 0)[0]
+        validation_dataloader = DataLoader(validation_dataset, batch_size = args.bs, collate_fn=lambda batch: collate_fn(batch, tokenizer=tokenizer, max_length=max_length))
+
+    end = time.perf_counter()
+    logger.info(f"- Dataset loading took {end-start} seconds.")
+    ####################################################################################################
+    # RUN ATTACK
+    ####################################################################################################
+    start = time.perf_counter()
+    logger.info("Running Attack")
+
+    # Initialize attack
+    ALoRaer = ALoRa(args.model_name, model_revision=args.model_revision, model_cache_dir=args.model_cache_dir)
+    
+    # Compute statistics
+    ALoRaer.load_model()
+    train_statistics_stepped, train_statistics_base = ALoRaer.compute_statistic(training_dataloader,learning_rate=args.lr,num_batches=math.ceil(args.num_samples/args.bs),device=device,model_half=args.model_half,accelerator=accelerator)
+    torch.save((train_statistics_stepped,train_statistics_base),f"{args.experiment_name}_train.pt")
+    val_statistics_stepped, val_statistics_base = ALoRaer.compute_statistic(validation_dataloader,learning_rate=args.lr,num_batches=math.ceil(args.num_samples/args.bs),device=device,model_half=args.model_half,accelerator=accelerator)
+    torch.save((val_statistics_stepped,val_statistics_base),f"{args.experiment_name}_val.pt")
+    ALoRaer.unload_model()
+
+    # Plot ROCs
+    ALoRaer.attack_plot_ROC(train_statistics_stepped/train_statistics_base, val_statistics_stepped/val_statistics_base, title=args.experiment_name, log_scale=False, show_plot=False)
+    ALoRaer.attack_plot_ROC(train_statistics_stepped/train_statistics_base, val_statistics_stepped/val_statistics_base, title=args.experiment_name, log_scale=True, show_plot=False)
+
+    end = time.perf_counter()
+
+    logger.info(f"- Experiment {args.experiment_name} took {end-start} seconds.")
+
+if __name__ == "__main__":
+    main()   
+
+
+
+"""
+Sample command line prompt (no acceleration): 
+python run_approx_lora.py --mod_size 70m --deduped --checkpoint step98000 --pack --n_samples 100 --lr 0.00005
+
+Note that this routine requires input pt's. The data input for this is a bit lossy because it goes:
+train: tokens -> string -> tokens [tokenizer.encode in this file]
+validation: string -> tokens (pack) -> string -> tokens [tokenizer.encode in this file]
+"""
+
+
+    # train_ids = [tokenizer.encode(t, return_tensors='pt') for t in training_dataset]
+    # val_ids = [tokenizer.encode(t, return_tensors='pt') for t in validation_dataset]
---
+++ src/attacks/ALoRa.py
@@ -0,0 +1,110 @@
+import os
+from transformers import AutoModelForCausalLM
+from .Attack import MIA
+from ..utils.attack_utils import *
+
+class ALoRa(MIA):
+    """
+    Approximate loss ratio thresholding attack (vs. pre-training)
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.model = None
+
+    @classmethod
+    def get_default_name(cls, model_name, model_revision, num_samples, seed):
+        """
+        Generates a default experiment name. Also ensures its validity with makedirs.
+
+        Args:
+            model_name (str): Huggingface model name
+            model_revision (str): model revision name
+            num_samples (int): number of training samples
+            seed (int): random seed
+        Returns:
+            string: informative name of experiment
+        """
+        os.makedirs("results/ALoRa", exist_ok=True)
+        return f"results/ALoRa/ALoRa_{model_name.replace('/','-')}_{model_revision.replace('/','-')}_N={num_samples}_seed={seed}"
+
+    def load_model(self):
+        """
+        Loads model into memory
+        """
+        if self.model is None:
+            self.model = AutoModelForCausalLM.from_pretrained(self.model_name, revision=self.model_revision, cache_dir=self.model_cache_dir)
+        else:
+            raise Exception("Model has already been loaded; please call .unload_model() first!")
+
+    def unload_model(self):
+        """
+        Unloads model from memory
+        """
+        self.model = None
+
+    def compute_statistic(self, dataloader, learning_rate, num_batches=None, device=None, model_half=None, accelerator=None):
+        """
+        Compute the approximate loss ratio statistic for a given dataloader.
+        Assumes batch size of 1.
+
+        Args:
+            dataloader (DataLoader): input data to compute statistic over
+            learning_rate (float): learning rate
+            num_batches (Optional[int]): number of batches of the dataloader to compute over.
+                If None, then comptues over whole dataloader
+            device (Optional[str]): e.g. "cuda"
+            model_half (Optional[bool]): whether to use model_half
+            accelerator (Optional[Accelerator]): accelerator object
+        Returns:
+            torch.Tensor: loss after the gradient descent step
+            torch.Tensor: loss before the gradient descent step
+        """
+        if self.model is None:
+            raise Exception("Please call .load_model() to load the model first.")
+        if dataloader.batch_size!=1:
+            raise Exception("ALoRa is only implemented for batch size 1")
+        
+        base_statistics = []
+        stepped_statistics = []
+        # Setup device
+        if accelerator is not None:
+            self.model, dataloader, = accelerator.prepare(self.model, dataloader)
+        else:
+            self.model.to(device)
+            if model_half:
+                self.model.half()
+        for batchno, data_x in tqdm(enumerate(dataloader),total=len(dataloader)):
+            if num_batches is not None and batchno >= num_batches:
+                break
+            # 1. Forward pass
+            data_x = data_x["input_ids"]
+            if accelerator is None:
+                data_x = data_x.to(device)
+            outputs = self.model(data_x, labels=data_x)
+            initial_loss = outputs.loss
+            initial_loss.backward() 
+            base_statistics.append(initial_loss.detach().cpu())
+
+            # 2. Perform gradient descent
+            with torch.no_grad():
+                for param in self.model.parameters():
+                    if param.grad is not None:
+                        param.add_(param.grad, alpha=-learning_rate)  # Gradient descent step
+
+            # 3. Compute the loss after update
+            output_after_ascent = self.model(data_x, labels=data_x)
+            new_loss = output_after_ascent.loss
+            stepped_statistics.append(new_loss.detach().cpu())
+
+            # 4. Restore model to original state
+            # self.unload_model()
+            # self.load_model()
+            with torch.no_grad():
+                for param in self.model.parameters():
+                    if param.grad is not None:
+                        param.add_(param.grad, alpha=learning_rate)  # Reverse gradient descent step
+            self.model.zero_grad()  # Reset gradients
+            del data_x, outputs, initial_loss, output_after_ascent, new_loss
+            torch.cuda.empty_cache()
+
+        return torch.tensor(stepped_statistics), torch.tensor(base_statistics)
