473d8b339e25614b9d98c2a7393fedb69a70748d
jsonW0 <jasonwang1@college.harvard.edu>
2024-05-25 12:51:49 -0400

    plot ci

--- src/llmprivacy/utils/plot_utils.py
+++ src/llmprivacy/utils/plot_utils.py
@@ -1,6 +1,8 @@
 import torch
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib as mpl
+from scipy.stats import bootstrap
 from sklearn.metrics import roc_curve, auc
 
 def plot_hist(train_statistics, val_statistics, plot_title, keep_first=None, show_plot=True, save_name=None, bins=20):
@@ -208,3 +210,92 @@ def print_AUC(train_statistic, val_statistic):
                                     torch.cat((-train_statistic,-val_statistic)).flatten())
     roc_auc = auc(fpr, tpr)
     return roc_auc
+
+def plot_ROC_multiple_ci(train_statistics_list, val_statistics_list, plot_title, labels, keep_first=None, show_plot=True, save_name=None, log_scale=False, fprs=None, colors=None):
+    '''
+    Plots multiple ROC curves in a single plot
+
+    Args:
+        train_statistics_list (list[list[float]]): list of curves, each a list of train statistics
+        val_statistics_list (list[list[float]]): list of curves, each a list of val statistics
+        plot_title (str): title of the plot
+        labels (list[str]): labels of each curve
+        fprs (list[float]): return TPRs at given FPRs. If unspecified, calculates at every 0.1 increment
+        keep_first (int): compute only for the first keep_first number of samples
+        show_plot (bool): whether to show the plot
+        save_name (str): save path for plot; does not save unless save_name is specified
+        log_scale (bool): whether to plot on log-log scale
+    
+    Returns:
+        roc_auc_map (dict[str,float]): map of labels to auc
+        tpr_at_fprs_map (dict[str,list[float]]): map of labels to the tprs at the given fprs
+    '''
+    if fprs is None:
+        fprs = [0.01,0.05,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9]
+    if colors is None:
+        colors = [mpl.colormaps["tab10"](i) for i in range(len(train_statistics_list))]
+
+    roc_auc_map = {}
+    tpr_at_fprs_map = {}
+    auc_se_map = {}
+    tpr_se_map = {}
+    plt.figure(figsize=(7,7),dpi=300)
+    plt.plot([0, 1], [0, 1], linestyle="--", c="k")
+    for i, (train_statistics, val_statistics, label) in enumerate(zip(train_statistics_list, val_statistics_list,labels)):
+        train_statistics = torch.as_tensor(train_statistics).flatten()[:keep_first]
+        train_statistics = train_statistics[~train_statistics.isnan()]
+        val_statistics = torch.as_tensor(val_statistics).flatten()[:keep_first]
+        val_statistics = val_statistics[~val_statistics.isnan()]
+
+        ground_truth = torch.cat((torch.ones_like(train_statistics),torch.zeros_like(val_statistics))).flatten()
+        predictions = torch.cat((-train_statistics,-val_statistics)).flatten()
+
+        fpr, tpr, thresholds = roc_curve(ground_truth,predictions)
+        roc_auc = auc(fpr, tpr)
+        
+        # Compute CI
+        fpr_range = np.linspace(0, 1, 2000)
+        def auc_statistic(data,axis):
+            ground_truth = data[0,0,:].T
+            predictions = data[1,0,:].T
+            fpr, tpr, thresholds = roc_curve(ground_truth, predictions)
+            roc_auc = auc(fpr, tpr)
+            tpr_range = np.interp(fpr_range,fpr,tpr)
+            return np.array([[roc_auc]+tpr_range.tolist()]).T
+        
+        data = torch.cat((ground_truth[:,None],predictions[:,None]),dim=1)
+        bootstrap_result = bootstrap((data,), auc_statistic, confidence_level=0.95, n_resamples=100, batch=1, method='percentile',axis=0)
+        
+        if not log_scale:
+            plt.plot(fpr, tpr, label=f'{label} (AUC = {roc_auc:0.4f})',c=colors[i])
+            plt.fill_between(fpr_range,bootstrap_result.confidence_interval.low[1:],bootstrap_result.confidence_interval.high[1:],alpha=0.1,color=colors[i])        
+            plt.xlim([0,1])
+            plt.ylim([0,1])
+        else:
+            plt.loglog(fpr, tpr, label=f'{label} (AUC = {roc_auc:0.4f})',c=colors[i])
+            plt.fill_between(fpr_range,bootstrap_result.confidence_interval.low[1:],bootstrap_result.confidence_interval.high[1:],alpha=0.1,color=colors[i])        
+            plt.xlim([0.001,1])
+            plt.ylim([0.001,1])
+        roc_auc_map[label] = roc_auc
+        tpr_at_fprs_map[label] = [tpr[np.max(np.argwhere(fpr<=fpr_val))] for fpr_val in fprs]
+        auc_se_map[label] = bootstrap_result.standard_error[0]
+        tpr_se_map[label] = [bootstrap_result.standard_error[np.max(np.argwhere(fpr_range<=fpr_val))] for fpr_val in fprs]
+    
+    plt.title(plot_title)
+    plt.legend(loc="lower right")
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.gca().set_aspect('equal', adjustable='box')
+    plt.minorticks_on()
+    plt.grid(which="major",alpha=0.2)
+    plt.grid(which="minor",alpha=0.1)
+    texts = plt.legend().get_texts()
+    # texts[4].set_fontweight('bold')
+    # texts[5].set_fontweight('bold')
+    if save_name is not None:
+        if save_name[-4:] != ".png" and save_name[-4:] != ".pdf":
+            save_name = save_name + ".png"
+        plt.savefig(save_name, bbox_inches="tight")
+    if show_plot:
+        plt.show()
+    return roc_auc_map, tpr_at_fprs_map, auc_se_map, tpr_se_map
